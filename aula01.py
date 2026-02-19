aeb0abef093180e3126606edd582630ef69363eb
rafael <duda.mra.farias@gmail.com>
2026-02-19 10:05:13 -0300

    primeira aula de programação

---
+++ aula01.py
@@ -0,0 +1,9 @@
+idade = 15 # variável inteira
+peso = 53.3 # variável float 
+nome = "Maria Eduarda Guedes Farias" # variável string
+sexo_feminino = True # variável bool 
+
+print(nome)
+print(idade)
+print(peso)
+print(sexo_feminino)
