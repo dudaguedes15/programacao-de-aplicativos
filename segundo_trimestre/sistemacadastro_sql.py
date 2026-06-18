7cd0c3687eaec40ac29df36effef1ae700bbf70f
duda guedes <duda.mra.farias@gmail.com>
2026-06-18 10:14:58 -0300

    18/06

--- segundo_trimestre/sistemacadastro_sql.py
+++ segundo_trimestre/sistemacadastro_sql.py
@@ -10,7 +10,11 @@ def cadastrar():
                 telefone TEXT,
                 turma TEXT,
                 idade INTERGER,
-                cpf TEXT UNIQUE NOT NULL
+                cpf TEXT UNIQUE NOT NULL,
+                id_professor INTEGER,
+                   
+                FOREIGN KEY (id_professor)
+                REFERENCES professor(id)
                 )
                 ''')
 
@@ -19,10 +23,11 @@ def cadastrar():
     turma_aluno = input (" Digite qual a sua turma: ")
     idade_aluno = int(input(" Digite a sua idade: "))
     cpf_aluno = input(" Digite seu CPF: ")
+    id_professor = int(input("Digite o ID do professor: "))
 
     comando_inserir = f'''
         INSERT INTO  alunos (nome, telefone, turma, idade, cpf)
-        values ('{nome_aluno}', '{telefone_aluno}', '{turma_aluno}', '{idade_aluno}', '{cpf_aluno}')
+        values ('{nome_aluno}', '{telefone_aluno}', '{turma_aluno}', '{idade_aluno}', '{cpf_aluno}', '{id_professor}')
         '''
 
     cursor.execute(comando_inserir)
@@ -50,6 +55,7 @@ def listar():
         print(f"Turma: {aluno[3]}")
         print(f"Idade: {aluno[4]}")
         print(f"CPF: {aluno[5]}")
+        print(f"ID do professor: {aluno[6]}")
         print("-" * 30)
 
 def alterar():
@@ -59,7 +65,7 @@ def alterar():
 
     id_aluno = int(input(" Qual seu ID: "))
 
-    cursor.execute(f'''SELECT nome , cpf , telefone , idade , turma FROM alunos WHERE id = {id_aluno}''')
+    cursor.execute(f'''SELECT nome , telefone , turma , idade , cpf , professor FROM alunos WHERE id = {id_aluno}''')
     
     aluno = cursor.fetchone()
 
@@ -67,16 +73,23 @@ def alterar():
         print(" Não encontrado ")
     else:
         print(f" Nome atual {aluno[0]} ")
-        print(f" CPF atual {aluno [1]} ")
-        print(f" Telefone atual {aluno[2]} ")
-        print(f" Idade atual {aluno [3]} ")
-        print(f" Turma atual {aluno [4]} ")
+        print(f" Telefone atual {aluno[1]} ")
+        print(f" Turma atual {aluno[2]} ")
+        print(f" Idade atual {aluno[4]} ")
+        print(f" CPF atual {aluno[5]} ")
+        print(f"Professor atual: {aluno[6]}")
+        
+        
 
         nome_atualizado = input(" Atualize seu nome: ")
-        cpf_atualizado = input(" Atualize seu CPF: ")
         telefone_atualizado = input(" Atualize se telefone: ")
-        idade_atualizada = input(" Atualize sua idade: ")
         turma_atualizada = input(" Atualize sua turma: ")
+        idade_atualizada = input(" Atualize sua idade: ")
+        cpf_atualizado = input(" Atualize seu CPF: ")
+        professor_id_atualizado = input("Atualize o ID do professor: ")
+        
+        
+        
 
         cursor.execute(f'''
                         UPDATE alunos
