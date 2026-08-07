8b1d08bd98798818d15f0ce96506386870ef2887
duda guedes <duda.mra.farias@gmail.com>
2026-08-07 10:13:41 -0300

    07/08

--- segundo_trimestre/revisao_02.py
+++ segundo_trimestre/revisao_02.py
@@ -28,10 +28,10 @@ def cadastrar():
         id_cinemas = int(input("Digite o ID do cinema: "))
 
         comando_inserir_cinemas = f'''INSERT INTO cinemas (nome, shopping) values ('{nome_cinema}', '{shopping}')'''
-        comando_inserir_salas = f'''INSERT INTO cinemas (numero_sala, capacidade, id_cinemas) values ('{numero_sala}', '{capacidade}', {id_cinemas}')'''
+        comando_inserir_salas = f'''INSERT INTO salas (numero_sala, capacidade, id_cinemas) values ('{numero_sala}', '{capacidade}', {id_cinemas})'''
 
-        comando.execute(comando_inserir_cinemas)
-        comando.execute(comando_inserir_hospitais)
+        cursor.execute(comando_inserir_cinemas)
+        cursor.execute(comando_inserir_salas)
 
 
         conexao.commit()
@@ -46,4 +46,57 @@ def cadastrar():
     finally:
         conexao.close()
 
-cadastrar()
+
+
+def listar():
+    try: 
+        conexao = sqlite3.connect('sistema_cinema.db')
+        cursor = conexao.cursor()
+
+        cursor.execute("SELECT * FROM salas")
+        salas = cursor.fetchall()
+
+        print("===== LISTA DE SALAS ===")
+
+        for sala in salas:
+            print
+            print(f"ID: {sala[0]}")
+            print(f"Número da sala: {sala [1]}")
+            print(f"Capacidade de pessoas: {sala[2]}")
+            print(f"ID do cinema: {sala[3]}")
+            print("-" * 30)
+
+    except Exception as e:
+        print(f"Ocorreu um erro: {e}.")
+    finally:
+        conexao.close()
+
+def menu():
+    try:
+        opcao = 0
+        while opcao != 5:
+
+            print("-------------------- MENU ------------------")
+            print("-- 1 Cadastrar cinema -- 2 Listar salas -- 3 Fechar programa -- ")
+            opcao = int(input("Digite o que você deseja fazer: "))
+        
+            if opcao == 1:
+                cadastrar()
+            elif opcao == 2:
+                listar()
+            elif opcao == 3:
+                print("---------- PROGRAMA ENCERRADO ----------")
+                break
+            
+            else:
+                print("Opção inválida.")
+        
+    except ValueError:
+        print("Erro de valor no cadastro tente novamente")
+    except TypeError:
+        print("Erro de tipo de dados")
+   
+    except Exception as e:
+        print(f"Ocorreu um erro: {e}.")
+
+menu()
