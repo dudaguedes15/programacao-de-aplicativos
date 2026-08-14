e058ca0d7bf6961c86ac232557ffcb5126d687e8
duda guedes <duda.mra.farias@gmail.com>
2026-08-14 08:31:48 -0300

    14/04

--- segundo_trimestre/sqlite_exercicios/prova-2-trimestre.py
+++ segundo_trimestre/sqlite_exercicios/prova-2-trimestre.py
@@ -36,7 +36,7 @@ def cadastrar():
         print("Cadastro realizado.")
 
     except sqlite3.IntegrityError as e:
-        print(f"Cadastro já realisado: {e}." )
+        print(f"Esse ID não existe: {e}." )
     except ValueError:
         print("Digite um valor válido.")
     except Exception as e:
@@ -101,8 +101,8 @@ def atualizar_sociedades():
         if not s:
             print("Não encontrado.")
         else: 
-            print(f"Nome atualizado da banca: {s[1]}")
-            print(f"Registro oab atualizado: {s[2]}")
+            print(f"Nome atual da banca: {s[1]}")
+            print(f"Registro oab atual: {s[2]}")
             
 
             nome_atualizado = input("Digite novo nome: ")
@@ -112,6 +112,7 @@ def atualizar_sociedades():
                                     SET nome_banca = '{nome_atualizado}', registro_oab_juridico = '{registro_atualizado}'
                                 WHERE id = {id_sociedades}   ''')
             conexao.commit()
+            print("Sociedade atualizada com sucesso.")
     except ValueError:
         print("Erro de valor no cadastro tente novamente")
     except TypeError:
@@ -135,8 +136,8 @@ def atualizar_filiais():
         if not f:
             print("Não encontrado.")
         else:
-            print(f"Estado atualizado: {f[1]}")
-            print(f"Id sociedades atualisado: {f[2]}")
+            print(f"Estado atual: {f[1]}")
+            print(f"Id sociedades atual: {f[2]}")
 
             estado_atualizado = input("Digite o novo eestado: ")
             id_sociedades_atualizado = int(input("Digite o novo ID: "))
@@ -147,8 +148,11 @@ def atualizar_filiais():
                                             WHERE id = {id_filiais}''')
 
             cursor.execute(f'''UPDATE filiais_regionais
-                                    SET estado_uf = '{estado_atualizado}', id_sociedades = '{id_sociedades_atualizado}'
+                                    SET estado_uf = '{estado_atualizado}'
+                                WHERE id_sociedades = '{id_sociedades_atualizado}'
                                     ''')
+            conexao.commit()
+            print("Filial atuzalizada com sucesso.")
     except ValueError:
         print("Erro de valor no cadastro tente novamente")
     except TypeError as e:
@@ -157,8 +161,85 @@ def atualizar_filiais():
     finally:
         conexao.close() 
 
-cadastrar()
-listar_sociedade()
-listar_filiais()
-atualizar_sociedades()
-atualizar_filiais()
+def excluir_sociedades():
+    try:
+        conexao = sqlite3.connect('sistema_escritorio_adv.db')
+        cursor = conexao.cursor()
+        listar_sociedade()
+
+        id_sociedades = int(input("Digite o ID do cadastro que você deseja deletar: "))
+
+        cursor.execute(f'''DELETE FROM sociedades_advogados WHERE id = {id_sociedades}''')
+
+        conexao.commit()
+        print("Sociedade excluida.")
+        conexao.close()
+    except ValueError:
+        print("Erro de valor no cadastro tente novamente")
+    except TypeError:
+        print("Erro de tipo de dados")
+    except Exception as e:
+        print(f"Ocorreu um erro: {e}.")
+    finally:
+        conexao.close()
+
+def excluir_filiais():
+    try:
+        conexao = sqlite3.connect('sistema_escritorio_adv.db')
+        cursor = conexao.cursor()
+        listar_filiais()
+
+        id_filiais = int(input("Digite o ID do cadastro que você deseja deletar: "))
+
+        cursor.execute(f'''DELETE FROM filiais_regionais WHERE id = {id_filiais}''')
+
+        conexao.commit()
+        print("Filial excluida.")
+        conexao.close()
+    except ValueError:
+        print("Erro de valor no cadastro tente novamente")
+    except TypeError:
+        print("Erro de tipo de dados")
+    except Exception as e:
+        print(f"Ocorreu um erro: {e}.")
+    finally:
+        conexao.close()
+
+def menu():
+    try:
+        opcao = 0
+        while opcao != 8:
+            print("\n")
+            print("-------------------- MENU ------------------")
+            print("-- 1 Cadastrar sociedades e filiais -- 2 Listar sociedade -- 3 Listar filiais --  4 Atualizar sociedades -- 5 Atualizar filiais -- 6 Excluir sociedades -- 7 Excluir filiais -- 8 Fechar programa --")
+            opcao = int(input("Digite o que você deseja fazer: "))
+
+            if opcao == 1:
+                cadastrar()
+            elif opcao == 2:
+                listar_sociedade()
+            elif opcao == 3:
+                listar_filiais()
+            elif opcao == 4:
+                atualizar_sociedades()
+            elif opcao == 5:
+                atualizar_filiais()
+            elif opcao == 6:
+                excluir_sociedades()
+            elif opcao == 7:
+                excluir_filiais()
+            elif opcao == 8:
+                print("===== PROGRAMA ENCERRADO =====")
+                break
+            else: 
+                print("Opção inválida.")
+        
+    except ValueError:
+        print("Erro de valor no cadastro tente novamente")
+    except TypeError:
+        print("Erro de tipo de dados")
+   
+    except Exception as e:
+        print(f"Ocorreu um erro: {e}.")        
+
+menu()
