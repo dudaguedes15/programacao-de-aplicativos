fae9972e0be2be31dce84e409b9351de0c22ddc6
duda guedes <duda.mra.farias@gmail.com>
2026-09-09 12:39:29 -0300

    09/09

--- segundo_trimestre/terceiro_trimestre/banco.py
+++ segundo_trimestre/terceiro_trimestre/banco.py
@@ -1,48 +1,49 @@
 import sqlite3
 
 def criar_tabela_escola():
-    try:
-        conexao = sqlite3.connect('gestao_escolar.db')
-        cursor = conexao.cursor()
 
-        cursor.execute("PRAGMA foreign_keys = ON")
-        cursor.execute('''
-                    CREAT TABLE IF NOT EXISTS escola(
-                        id  INTEGER PRIMARY KEY AUTOINCREMENT,
-                        nome TEXT NOT NULL,
-                        cidade TEXT NOT NULL)
-                        ''')
+    conexao = sqlite3.connect('gestao_escolar.db')
+    cursor = conexao.cursor()
 
-conexao.commit()
-conexao.close()
+    cursor.execute("PRAGMA foreign_keys = ON")
+    cursor.execute('''
+                CREATE TABLE IF NOT EXISTS escola(
+                    id  INTEGER PRIMARY KEY AUTOINCREMENT,
+                    nome TEXT NOT NULL,
+                    cidade TEXT NOT NULL)
+                    ''')
+    conexao.commit()
+    conexao.close()
 
 def criar_tabela_turma():
-    try:
-        conexao = sqlite3.connect('gestao_escolar.db')
-        cursor = conexao.cursor()
 
-        cursor.execute('''
-                    CREAT TABLE IF NOT EXISTS turma(
-                    id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    nome_turma TEXT NOT NULL,
-                    id_escola INTEGER,
-                    FOREING KEY (id_sociedades) REFERENCES escola (id))
-                    ''')
-conexao.commit()
-conexao.close()
+    conexao = sqlite3.connect('gestao_escolar.db')
+    cursor = conexao.cursor()
+
+    cursor.execute('''
+                CREATE TABLE IF NOT EXISTS turma(
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                nome_turma TEXT NOT NULL,
+                id_escola INTEGER,
+                FOREING KEY (id_sociedades) REFERENCES escola (id))
+                ''')
+    conexao.commit()
+    conexao.close()
 
 def criar_tabela_alunos():
-    try:
-        conexao = sqlite3.connect('gestao_escolar.db')
-        cursor = conexao.cursor()
 
-        cursor.execute('''
-                    CREAT TABLE IF NOT EXISTS alunos(
-                    id INTEGER PRIMARY KEY AUTOINCREMENT
-                    nome TEXT NOT NULL,
-                    idade INTEGER,
-                    id_turma INTEGER,
-                    FOREING KEY (id_turma) REFERENCES turma (id))
-                    ''')
-conexao.commit()
-conexao.close()
+    conexao = sqlite3.connect('gestao_escolar.db')
+    cursor = conexao.cursor()
+
+    cursor.execute('''
+                CREATE TABLE IF NOT EXISTS alunos(
+                id INTEGER PRIMARY KEY AUTOINCREMENT
+                nome TEXT NOT NULL,
+                idade INTEGER,
+                id_turma INTEGER,
+                FOREING KEY (id_turma) REFERENCES turma (id))
+                ''')
+    conexao.commit()
+    conexao.close()
+
+criar_tabela_escola()
--- segundo_trimestre/terceiro_trimestre/escola.py
+++ segundo_trimestre/terceiro_trimestre/escola.py
@@ -1,2 +1,50 @@
+import sqlite3
 from banco import criar_tabela_escola
 
+def cadastrar():
+    conexao = None
+    try:
+        conexao = sqlite3.connect("gestao_escolar.db")
+        cursor = conexao.cursor()
+
+        nome_escola = input("Digite o nome da escola: ")
+        cidade_escola = input("Digite a cidade da escola: ")
+
+        assert nome_escola != "", "O nome da escola não pode estar vazio."
+        assert cidade_escola != "", "A cidade da escola não pode estar vazia."
+        
+        comando_inserir_escola = (f'''INSERT INTO escola (nome, cidade) values ('{nome_escola}', '{cidade_escola}')''')
+        
+        cursor.execute(comando_inserir_escola)
+        conexao.commit()
+        print("Cadastro realizado.")
+    except sqlite3.IntegrityError as e:
+        print(f"Esse ID não existe: {e}." )
+    except ValueError:
+        print("Digite um valor válido.")
+    except Exception as e:
+        print(e)
+    finally:
+        conexao.close()
+
+def listar():
+    try:
+        conexao = sqlite3.connect('gestao_escolar.db')
+        cursor = conexao.cursor()
+
+        cursor.execute("SELECT * FROM escola")
+        escola = cursor.fetchall()
+
+        print("===== LISTA DE CADASTROS ESCOLA =====")
+        
+        for s in escola:
+            print(f"ID {s[0]}")
+            print(f"Nome da escola {s[1]}")
+            print(f"Cidade da escola {s[2]}")
+    except Exception as e:
+        print(f"Ocorreu um erro: {e}.")
+    finally:
+        conexao.close()
+cadastrar()
+listar()
+    
